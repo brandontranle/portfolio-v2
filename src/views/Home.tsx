4b249366a83c7adb19d5226017aaf72a85880bf2
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2024-05-29 22:33:02 -0700

    [style] -  margins

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -8,7 +8,7 @@ export const Home = () => {
         return () => setFadeIn(false); 
     }, []);
     return (
-        <div className={`text-right w-[450px] ml-auto text-sm md:text-lg ${fadeIn ? 'fade-in' : ''}`}>            
+        <div className={`text-right w-[450px] ml-auto text-sm md:text-lg  mt-auto lg:mt-0 ${fadeIn ? 'fade-in' : ''}`}>            
         <h1 className="font-barcode text-4xl md:text-5xl lg:text-6xl md:mb-5 mt-auto ml-auto"> Home</h1>
             <p className="mb-3"> <span className="dot"></span> <span className="text-black  dark:text-white  font-bold"> Status: </span> <a href="https://vitametrics.org"> Operating Vitametrics for alpha testing!  </a> </p>
             <p className="mb-3"> <span className="font-bold text-black  dark:text-white"> Outside of Work: </span> Runner, Pianist, Tech Enthusiast, Gamer.</p>
