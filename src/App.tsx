ae4100cc4b9b75333d1c5a4b0c4b43f264373122
Brandon Tran Le <111268644+brandontranle@users.noreply.github.com>
2024-06-22 04:10:36 -0700

    [chore] - added fluid animations

--- src/App.tsx
+++ src/App.tsx
@@ -68,12 +68,12 @@ function App() {
       <div id="inner-content" className={`flex-col bg-transparent h-full w-full px-0 py-0 md:p-5 xl:pt-8  xl:px-20 2xl:py-10  inner-content`}>
       <div id="parent" className="flex h-full"> 
       <div id="options" className="hidden xl:flex h-full flex-col rotate-[-180deg] gap-5 mb-5 font-neuzeitRegular w-[25px]">
-        <div onClick={() => toggleTheme("light")} className="flex flip-vertical hover:cursor-pointer gap-1 items-center"> Light <div className={`circle ${theme === "light" ? "selected-circle" : ""}`}/> </div>
-        <div onClick={() => toggleTheme("dark")} className="flip-vertical hover:cursor-pointer flex gap-1 items-center"> Dark <div className={`circle ${theme === "dark" ? "selected-circle" : ""}`}/>  </div>
-        <div onClick={() => toggleColorMode()} className="flip-vertical hover:cursor-pointer flex gap-1 items-center"> Color <div className={`circle ${colorMode ? "color-selected" : "color-unselected"}`}/>  </div>
+        <div onClick={() => toggleTheme("light")} className="flex flip-vertical hover:cursor-pointer gap-1 items-center mode-option"> Light <div className={`circle ${theme === "light" ? "selected-circle" : ""}`}/> </div>
+        <div onClick={() => toggleTheme("dark")} className="mode-option flip-vertical hover:cursor-pointer flex gap-1 items-center"> Dark <div className={`circle ${theme === "dark" ? "selected-circle" : ""}`}/>  </div>
+        <div onClick={() => toggleColorMode()} className="mode-option flip-vertical hover:cursor-pointer flex gap-1 items-center"> Color <div className={`circle ${colorMode ? "color-selected" : "color-unselected"}`}/>  </div>
       </div>
         <div className="content-area p-5 md:p-8 lg:p-10 relative z-10 md:pb-2 h-full w-full flex flex-col before:hidden md:before:block font-neuzeitRegular flex-shrink-0">
-          <h1 id="name" className="text-3xl mb-0 md:text-5xl xl:text-7xl md:mb-5 font-neuzeitBold">Brandon Le</h1>
+          <h1 id="name" className="text-3xl mb-0 md:text-5xl xl:text-7xl md:mb-5 font-neuzeitBold"> <a href="/"> Brandon Le</a> </h1>
           <h2 id="title" className="text-xs mb-0 md:text-2xl xl:text-4xl md:mb-3">S O F T W A R E &nbsp; E N G I N E E R</h2>
           {/*<p className="hidden md:text-lg md:block md:mb-1"> Listened to Enchanted, by Taylor Swift, 8 hours ago. </p>*/}
           <nav className="flex flex-col gap-2 md:gap-0 max-w-[100px] mt-4 md:mt-0">
